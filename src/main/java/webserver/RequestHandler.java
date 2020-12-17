69be49a287ecaacefb4de0240d3212125084f593
zino <zino@zino-PC>
2020-12-18 07:59:54 +0900

    ...

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,11 +10,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
+
+import model.User;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
 	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -54,20 +61,34 @@ public class RequestHandler extends Thread {
 			InputStreamReader reader = new InputStreamReader(in);
 			BufferedReader br = new BufferedReader(reader);
 			
-			String line = br.readLine();
-			String[] tokens = line.split(" ");
+			String requestLine = br.readLine();
+			String[] tokens = requestLine.split(" ");
+			String method = tokens[0];
 			String url = tokens[1];
-			
-			System.out.println(line);
-			
-			while ((line = br.readLine()) != null && !"".equals(line)) {
-				System.out.println(line);
-			}
+			String httpVersion = tokens[2];
 			
 			byte[] body = null;
+			User user = null;
+			Map<String, String> headerMap = null;
 			
-			if (url.equals("/index.html")) {
+			if (url.equals("/index.html") || url.equals("/user/form.html")) {
 				body = Files.readAllBytes(new File("./webapp" + url).toPath());
+			} else if (url.startsWith("/user/create")) {
+				
+				if ("GET".equals(method)) {
+					int index = url.indexOf("?");
+					String requestPath = url.substring(0, index);
+					String params = url.substring(index+1);
+					user = parseUser(params);
+				} else if ("POST".equals(method)) {
+					headerMap = getHeader(br);
+					int contentLength = Integer.parseInt(headerMap.get("Content-Length"));
+					String params = getBody(br, contentLength);
+					user = parseUser(params);
+				}
+				
+				System.out.println(user.toString());
+				
 			} else {
 				body = "Hello ZINO".getBytes();
 			}
@@ -80,20 +101,29 @@ public class RequestHandler extends Thread {
 		}
 	}
 
-	/* 가장 기본영
-	public void run() {
-		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(), connection.getPort());
-
-		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-			DataOutputStream dos = new DataOutputStream(out);
-			byte[] body = "Hello ZINO".getBytes();
-			response200Header(dos, body.length);
-			responseBody(dos, body);
-		} catch (IOException e) {
-			log.error(e.getMessage());
+	private Map<String, String> getHeader(BufferedReader br) throws IOException {
+		Map<String, String> map = new HashMap<>();
+		
+		String line = null;
+		while ((line = br.readLine()) != null && !Strings.isNullOrEmpty(line)) {
+			String[] arr = line.split("[:][ ]");
+			String key = arr[0];
+			String value = arr[1];
+			
+			map.put(key, value);
 		}
-	}*/
+		return map;
+	}
+	
+	private String getBody(BufferedReader br, int contentLength) throws IOException {
+		return IOUtils.readData(br, contentLength);
+	}
+	
+	private User parseUser(String params) {
+		Map<String, String> map = util.HttpRequestUtils.parseQueryString(params);
+		return new User( map.get("userId"), map.get("password"), map.get("name"), "");
+	}
+	
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
 		try {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
---
+++ src/test/java/util/ZinoTest.java
@@ -0,0 +1,76 @@
+package util;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.google.common.base.Strings;
+
+import model.User;
+
+public class ZinoTest {
+
+	@Test
+	public void post_header() {
+		String str = "Content-Length: 59";
+		String[] arr = str.split("[:][ ]");
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+	}
+	
+	/* 문자열을 Split 할 떄는 delimiter를 []로 감싸서 명확하게 표현한다.  */
+	public void 물음표_text() {
+		String str = "/user/create?userId=javajigi&password=password&name=JaeSung";
+		String[] arr = str.split("[?]");
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+	}
+	
+	/* null이나 empty체크용 유틸은 이미 있었다. */
+	public void 문자열_체크() {
+		assertTrue(Strings.isNullOrEmpty(""));
+	}
+	
+	public void http리퀘스트_파싱() {
+		String queryStr = "userId=javajigi&password=password&name=JaeSung";
+		Map<String, String> map = util.HttpRequestUtils.parseQueryString(queryStr);
+		
+		Map<String, Object> expected = new HashMap<>();
+		expected.put("userId", "javajigi");
+		expected.put("password", "password");
+		expected.put("name", "JaeSung");
+		
+		assertTrue(expected.equals(map));
+	}
+
+	/* 두 Map의 엔트리를 비교할 때는 map.equals()를 사용한다. */
+	public void 회원가입_파싱() {
+		String str = "/user/create?userId=javajigi&password=password&name=JaeSung";
+		
+		String[] arr = str.split("[?]");
+		String url = arr[0];
+		String queryString = arr[1];
+		String[] params = queryString.split("[&]");
+		
+		Map<String, Object> user = new HashMap<>();
+		
+		for (int i=0; i < params.length; i++) {
+			String paramStr = params[i];
+			String[] paramArr = paramStr.split("=");
+			String key = paramArr[0];
+			String value = paramArr[1];
+			
+			user.put(key, value);
+		}
+		
+		Map<String, Object> expected = new HashMap<>();
+		expected.put("userId", "javajigi");
+		expected.put("password", "password");
+		expected.put("name", "JaeSung");
+		
+		assertTrue(expected.equals(user));
+	}
+}
