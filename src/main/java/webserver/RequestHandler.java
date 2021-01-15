531fa78f51da8f27594ca6883a1ddd4723885a77
zino <zino@zino-PC>
2021-01-16 01:54:46 +0900

    요구사항 6 - 사용자 목록 출력 (로그인시)

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,7 +9,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -66,7 +68,7 @@ public class RequestHandler extends Thread {
 				// 정상적으로 응답한다.
 				response200Header(dos, body.length);
 				responseBody(dos, body);
-			} else if (url.startsWith("/user/create")) {
+			} else if (url.equals("/user/create")) {
 
 				Map<String, String> paramMap = null;
 
@@ -118,7 +120,7 @@ public class RequestHandler extends Thread {
 				// 6. URL 리다이렉션을 통해 브라우저에 남아있는 회원가입 정보를 재사용하기 어렵게 처리한다.
 				response302Header(dos);
 				dos.flush();
-			} else if (url.startsWith("/user/login")) {
+			} else if (url.equals("/user/login")) {
 
 				Map<String, String> paramMap = null;
 
@@ -173,6 +175,44 @@ public class RequestHandler extends Thread {
 				// 6. URL 리다이렉션을 통해 로그인 결과를 보여준다.
 				response302HeaderWithLocationAndCookie(dos, logined, location);
 				dos.flush();
+			} else if (url.equals("/user/list")) {
+				Map<String, String> cookieMap = null;
+
+				//1. 로그인 정보를 헤더에서 뽑는다.
+				String line = null;
+				while ((line = br.readLine()) != null && !line.isBlank()) {
+					String[] arr = line.split(": ");
+					if ("Cookie".equals(arr[0])) {
+						cookieMap = HttpRequestUtils.parseCookies(arr[1]);
+					}
+				}
+
+				boolean logined = false;
+				if (cookieMap != null) {
+					logined = Boolean.parseBoolean(cookieMap.get("logined"));
+				}
+
+				if (logined) {
+					log.debug("=== 로그인이라서 사용자를 출력합니다.");
+					//2. 로그인 상태라면 사용자 목록을 불러온다. 사용자 목록을 출력한다.
+					Collection<User> collection = DataBase.findAll();
+					Iterator<User> it = collection.iterator();
+					
+					StringBuilder sb = new StringBuilder();
+					while (it.hasNext()) {
+						User user = it.next();
+						sb.append(user.toString());
+					}
+					
+					body = sb.toString().getBytes();
+					response200HeaderWithCookie(dos, logined, body.length);
+					responseBody(dos, body);
+				} else {
+					//3. 비로그인 상태라면 로그인페이지로 이동한다.
+					String location = "/user/login.html";
+					log.debug("=== 비로그인이라서 로그인페이지로 이동합니다.");
+					response302HeaderWithLocation(dos, location);
+				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -194,12 +234,33 @@ public class RequestHandler extends Thread {
 			log.error(e.getMessage());
 		}
 	}
+	private void response302HeaderWithLocation(DataOutputStream dos, String url){
+		try {
+			dos.writeBytes("HTTP/1.1 302 OK \r\n");
+			dos.writeBytes("Location: " + url + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e ) {
+			log.error(e.getMessage());
+		}
+	}
 
 	private void response302HeaderWithLocationAndCookie(DataOutputStream dos, boolean logined, String location){
 		try {
 			dos.writeBytes("HTTP/1.1 302 OK \r\n");
 			dos.writeBytes("Location: " + location +" \r\n");
 			dos.writeBytes("Set-Cookie: logined=" + logined +" \r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e ) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private void response200HeaderWithCookie(DataOutputStream dos, boolean logined, int contentLength){
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Set-Cookie: logined=" + logined +" \r\n");
+			dos.writeBytes("Content-Length: " + contentLength +" \r\n");
 			dos.writeBytes("\r\n");
 		} catch (IOException e ) {
 			log.error(e.getMessage());
